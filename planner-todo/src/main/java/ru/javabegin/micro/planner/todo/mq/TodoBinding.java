d0c126dd7210942a8d298f2220962f6303901191
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-10-05 20:34:14 +0300

    fix misspell

--- planner-todo/src/main/java/ru/javabegin/micro/planner/todo/mq/TodoBinding.java
+++ planner-todo/src/main/java/ru/javabegin/micro/planner/todo/mq/TodoBinding.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.messaging.MessageChannel;
 
 public interface TodoBinding {
-    String INPUT_CHANNEL = "todoInputPutChannel";
+    String INPUT_CHANNEL = "todoInputChannel";
     @Input(INPUT_CHANNEL)
     MessageChannel todoInputChannel();
 }
--- planner-users/src/main/java/ru/javabegin/micro/planner/users/mq/TodoBinding.java
+++ planner-users/src/main/java/ru/javabegin/micro/planner/users/mq/TodoBinding.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
 
 public interface TodoBinding {
-    String OUTPUT_CHANNEL = "todoOutPutChannel";
+    String OUTPUT_CHANNEL = "todoOutputChannel";
     @Output(OUTPUT_CHANNEL)
     MessageChannel todoOutputChannel();
 }
